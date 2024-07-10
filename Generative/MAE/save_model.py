5702b4216c5c27dc8f180def35d91ce10c4d4c52
sarapeddy <269362@studenti.unimore.it>
2024-07-10 21:34:07 +0200

    Change format in pkl

--- Generative/MAE/save_model.py
+++ Generative/MAE/save_model.py
@@ -4,15 +4,15 @@ import torch
 
 def save_model(args, model, optimizer):
     # out = os.path.join(args.model_path, "checkpoint_{}.tar".format(args.current_epoch))
-    out = os.path.join(f'./{args.model_path}', "Pretrained_{}_{}.tar".format(
-            args.dataset, args.emb_dim))
+    # out = os.path.join(f'./{args.model_path}/forecasting', "Pretrained_{}_{}.pkl".format(
+    #         args.dataset, args.emb_dim))
 
-    if not os.path.exists(out):
-        os.makedirs(out, exist_ok=True)
+    if not os.path.exists(f'./{args.model_path}/forecasting/Pretrained_{args.dataset}_{args.emb_dim}'):
+        os.makedirs(f'./{args.model_path}/forecasting/Pretrained_{args.dataset}_{args.emb_dim}', exist_ok=True)
 
     # To save a DataParallel model generically, save the model.module.state_dict().
     # This way, you have the flexibility to load the model any way you want to any device you want.
     if isinstance(model, torch.nn.DataParallel):
-        torch.save(model.module.state_dict(), out)
+        torch.save(model.module.state_dict(), f'./{args.model_path}/forecasting/Pretrained_{args.dataset}_{args.emb_dim}.pkl')
     else:
-        torch.save(model.state_dict(), out)
+        torch.save(model.state_dict(), f'./{args.model_path}/forecasting/Pretrained_{args.dataset}_{args.emb_dim}.pkl')
