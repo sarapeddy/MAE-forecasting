87207dc06aa6a91de392f1ec4bf3c4421df4e4db
sarapeddy <269362@studenti.unimore.it>
2024-07-12 19:59:59 +0200

    Add the dataset for noraml finetuning forecasting

--- Generative/MAE/finetune_train_tensor_dataset.py
+++ Generative/MAE/finetune_train_tensor_dataset.py
@@ -27,11 +27,11 @@ class TimeSeriesDatasetWithMovingAvg_Finetune(TensorDataset):
         self.data_y = torch.swapaxes(self.data_y, 0,1)
         expanded_dataset = torch.cat([self.x_time, self.x_avg, self.x_err], dim=2)
 
-        if self.mode == 'train':
-            n_samples = int(labelled_ratio * expanded_dataset.shape[0])
-            indices = torch.randperm(expanded_dataset.shape[0])[:n_samples]
-            expanded_dataset = expanded_dataset[indices]
-            self.data_y = self.data_y[indices]
+        # if self.mode == 'train':
+        #     n_samples = int(labelled_ratio * expanded_dataset.shape[0])
+        #     indices = torch.randperm(expanded_dataset.shape[0])[:n_samples]
+        #     expanded_dataset = expanded_dataset[indices]
+        #     self.data_y = self.data_y[indices]
 
         super(TimeSeriesDatasetWithMovingAvg_Finetune, self).__init__(expanded_dataset)
 
@@ -47,6 +47,41 @@ class TimeSeriesDatasetWithMovingAvg_Finetune(TensorDataset):
         x_avg = torch.cat([time_data, avg_data], dim=1)
         x_err = torch.cat([time_data, err_data], dim=1)
         return x_avg, x_err, data_y
+
+    def __len__(self):
+        return len(self.tensors[0])
+
+
+
+class TimeSeriesDataset_Finetune(TensorDataset):
+    def __init__(self, original_dataset: Tensor, n_time_cols,  kernel_size=9, seq_len=336, pred_len=24, labelled_ratio=0.1, mode='train'):
+        self.n_time_cols = n_time_cols
+        self.mode = mode
+        self.seq_len = seq_len
+        self.pred_len = pred_len
+
+        self.x_original = original_dataset[:, :, self.n_time_cols:]
+
+        self.original_dataset = original_dataset[:, :-self.pred_len, :].unfold(dimension=1, size=self.seq_len, step=1)
+        self.data_y = self.x_original[:, self.seq_len:, :].unfold(dimension=1, size=self.pred_len, step=1)
+
+        self.original_dataset = torch.swapaxes(self.original_dataset, 0,1)
+        self.data_y = torch.swapaxes(self.data_y, 0,1)
+
+        # if self.mode == 'train':
+        #     n_samples = int(labelled_ratio * expanded_dataset.shape[0])
+        #     indices = torch.randperm(expanded_dataset.shape[0])[:n_samples]
+        #     expanded_dataset = expanded_dataset[indices]
+        #     self.data_y = self.data_y[indices]
+
+        super(TimeSeriesDataset_Finetune, self).__init__(self.original_dataset)
+
+    def __getitem__(self, index):
+
+        data_y = self.data_y[index]
+        el = self.tensors[0][index]
+
+        return el, data_y
 
     def __len__(self):
         return len(self.tensors[0])
